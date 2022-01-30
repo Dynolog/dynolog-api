a1b6e6d305a14e3986167417b1ab46389739fee1
Renato Henrique <renato.henrique_98@hotmail.com>
2022-01-30 16:06:42 -0300

    feat: New Timezones controller

---
+++ src/main/java/com/github/appointmentsio/api/controllers/TimezonesController.java
@@ -0,0 +1,26 @@
+package com.github.appointmentsio.api.controllers;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.TimeZone.getAvailableIDs;
+
+
+@RestController
+@Tag(name = "Projects")
+@RequestMapping("api/timezones")
+@SecurityRequirement(name = "token")
+@PreAuthorize("hasAnyAuthority('USER')")
+public class TimezonesController {
+
+    @GetMapping
+    public List<String> index() {
+        return List.of(getAvailableIDs());
+    }
+}
